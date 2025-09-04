28678d8c0eedeb572d5b4c151663dc582fbbc381
Ben Willan <93150547+benWillan@users.noreply.github.com>
2025-09-04 13:03:20 +0100

    [feat] Add side bar
    Basic needs more styling.

--- WebApi/ClientApp/src/App.tsx
+++ WebApi/ClientApp/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { NavBar } from './components/topLevel/NavBar'
 import { ContainerFluid } from './components/topLevel/ContainerFluid';
+import Sidebar from './components/general/Sidebar'
 
 function App() {
   return (
     <>
       <NavBar></NavBar>
+      <Sidebar></Sidebar>
       <ContainerFluid chartsRendered={2}></ContainerFluid>
     </>
   );
---
+++ WebApi/ClientApp/src/components/general/Sidebar.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+import {Button} from "react-bootstrap";
+
+const Sidebar = () => {
+  return (
+    <div
+      className="bg-body-tertiary"
+      style={{
+        height: "100vh",         // Full height
+        width: "56px",          // Sidebar width
+        position: "fixed",       // Stick to side
+        top: 0,
+        right: 0,                 // Change to "right: 0" if you want it on the right
+        backgroundColor: "#343a40", // Bootstrap dark color
+        paddingTop: "20px",
+        display: "flex",
+        flexDirection: "column",
+        boxShadow: "2px 0 5px rgba(0,0,0,0.2)",
+        zIndex: 1000,
+      }}
+    >
+      {/* <h4
+        style={{
+          color: "#fff",
+          textAlign: "center",
+          marginBottom: "20px",
+        }}
+      >
+        My App
+      </h4> */}
+      <Nav className="flex-column" variant="pills">
+        <Nav.Link href="/" className="text-white">
+          <Button style={{width: "80%"}}>C</Button>
+        </Nav.Link>
+        <Nav.Link href="/about" className="text-white">About</Nav.Link>
+        <Nav.Link href="/services" className="text-white">Services</Nav.Link>
+        <Nav.Link href="/contact" className="text-white">Contact</Nav.Link>
+      </Nav>
+    </div>
+  );
+};
+
+export default Sidebar;
