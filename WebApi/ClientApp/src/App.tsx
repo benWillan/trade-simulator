d96ee3bc375080b163ea58aa42270f0885d79621
Ben Willan <93150547+benWillan@users.noreply.github.com>
2025-09-30 23:54:16 +0100

    [feature] Add Date picker and load button

--- WebApi/ClientApp/src/App.tsx
+++ WebApi/ClientApp/src/App.tsx
@@ -3,7 +3,7 @@ import { NavBar } from './components/appLevel/NavBar';
 import { ContentArea } from './components/appLevel/ContentArea';
 import Sidebar from './components/general/Sidebar';
 import OffCanvas from './components/general/OffCanvas';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CompareModal from './components/appLevel/CompareModal';
 import Toast from './components/general/Notification';
 
@@ -11,12 +11,31 @@ function App() {
 
   const [offCanvasVisibility, setOffCanvasVisibility] = useState<boolean>(false);
   
+  const [datePickerValue, setDatePickerValue] = useState<string>('');
+  
   const showOffCanvas = () => setOffCanvasVisibility(true);
   const hideOffCanvas = () => setOffCanvasVisibility(false);
 
+  const setStartDateState = (startDate: string) => {
+
+    setDatePickerValue(startDate);
+
+  }
+
+  // const isValidDate = (value: string): boolean => {
+
+  //   if (value.length !== 10) return false;
+  //   if (!/^\d{4}-\d{2}-\d{2}$/.test(value)) return false;
+
+  //   const date = new Date(value);
+  //   return !isNaN(date.getTime()) && value === date.toISOString().slice(0, 10);
+
+  // }
+
+
   return (
     <>
-      <NavBar></NavBar>
+      <NavBar startDateValue={datePickerValue} onStartDateSet={setStartDateState}></NavBar>
       <ContentArea onWatchListShow={showOffCanvas} chartsRendered={1} isOffCanvasVisible={offCanvasVisibility}></ContentArea>
       <OffCanvas visibility={offCanvasVisibility} onWatchListHide={hideOffCanvas} ></OffCanvas>
     </>
--- WebApi/ClientApp/src/components/appLevel/NavBar.tsx
+++ WebApi/ClientApp/src/components/appLevel/NavBar.tsx
@@ -9,18 +9,36 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import NavItem from 'react-bootstrap/NavItem';
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
-export function NavBar() {
+type Props = {
+  startDateValue: string;
+  onStartDateSet: (startDate: string) => void;
+}
+
+export function NavBar({startDateValue, onStartDateSet}: Props) {
+
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
 
   return (
     <Navbar style={{zIndex: 2}} bg="dark" data-bs-theme="dark" className="bg-body-tertiary px-2">
       <Navbar.Brand href="#home"><span style={{fontFamily: "Verdana", fontSize: 18}}>Trade Simulator</span></Navbar.Brand>
       <Nav className="mx-auto">
+      <Button 
+        variant={isPlaying === true ? "outline-secondary" : "outline-success"}
+        onClick={() => setIsPlaying(!isPlaying)}
+        className="me-2 rounded-pill"
+      >
+        {isPlaying === true ? "=" : ">"}
+      </Button>
       <Form className="d-flex">
-        <Button variant="outline-success" className="me-2">&gt;</Button>
         <Form.Control
+          className='ms-4'
           type="date"
+          value={startDateValue || ""}
+          onChange={(e) => onStartDateSet(e.target.value)}
         />
+        <Button type='button' variant="secondary" className="ms-4" size='sm' style={{height: "34px"}}>Load</Button>
       </Form>
       </Nav>
     </Navbar>
