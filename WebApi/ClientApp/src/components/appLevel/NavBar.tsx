d75d57443b916bc1120a83fac0da21dde829ee26
Ben Willan <93150547+benWillan@users.noreply.github.com>
2025-09-30 18:24:05 +0100

    [feat] Add play button

--- WebApi/ClientApp/src/components/appLevel/NavBar.tsx
+++ WebApi/ClientApp/src/components/appLevel/NavBar.tsx
@@ -5,30 +5,25 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import NavItem from 'react-bootstrap/NavItem';
+import Button from 'react-bootstrap/Button';
 
 export function NavBar() {
 
   return (
     <Navbar style={{zIndex: 2}} bg="dark" data-bs-theme="dark" className="bg-body-tertiary px-2">
       <Navbar.Brand href="#home"><span style={{fontFamily: "Verdana", fontSize: 18}}>Trade Simulator</span></Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
+      <Nav className="mx-auto">
+        <Button
+          variant="outline-success"
+          style={{ borderRadius: "20px" }}
+          className="position-absolute top-50 start-50 translate-middle"
+        >
+          &gt;
+        </Button>
+      </Nav>
     </Navbar>
   );
 }
