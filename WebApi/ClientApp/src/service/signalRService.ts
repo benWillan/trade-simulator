88d58b1b24e041dd3e16fb6604e76f798a7c8776
Ben Willan <93150547+benWillan@users.noreply.github.com>
2025-11-02 23:47:53 +0000

    [chore] added https:// to the .env base url string.

--- WebApi/ClientApp/src/service/signalRService.ts
+++ WebApi/ClientApp/src/service/signalRService.ts
@@ -1,48 +1,48 @@
-import * as signalR from "@microsoft/signalr";
+//import * as signalR from "@microsoft/signalr";
 
-class StockSignalRService {
+// class StockSignalRService {
 
-  private connection: signalR.HubConnection | null = null;
+//   private connection: signalR.HubConnection | null = null;
 
-  async connect(): Promise<void> {
+//   async connect(): Promise<void> {
 
-    this.connection = new signalR.HubConnectionBuilder()
-      .withUrl("https://localhost:7133/stockHub") // adjust port to your API
-      .withAutomaticReconnect()
-      .build();
+//     this.connection = new signalR.HubConnectionBuilder()
+//       .withUrl("https://localhost:7133/stockHub") // adjust port to your API
+//       .withAutomaticReconnect()
+//       .build();
 
-    this.connection.on("ReceiveStockTick", (tick) => {
+//     this.connection.on("ReceiveStockTick", (tick) => {
 
-      console.log("Received Tick:", tick);
+//       console.log("Received Tick:", tick);
       
-    });
+//     });
 
-    this.connection.on("Error", (msg) => {
+//     this.connection.on("Error", (msg) => {
 
-      console.error("Error:", msg);
+//       console.error("Error:", msg);
 
-    });
+//     });
 
-    await this.connection.start();
-    console.log("Connected to SignalR Hub");
+//     await this.connection.start();
+//     console.log("Connected to SignalR Hub");
 
-  }
+//   }
 
-  async startSimulation(symbol: string, startTime: string): Promise<void> {
+//   async startSimulation(symbol: string, startTime: string): Promise<void> {
 
-    if (!this.connection) return;
+//     if (!this.connection) return;
 
-    await this.connection.invoke("StartSimulation", symbol, startTime, 1000);
+//     await this.connection.invoke("StartSimulation", symbol, startTime, 1000);
 
-  }
+//   }
 
-  async stopSimulation(): Promise<void> {
+//   async stopSimulation(): Promise<void> {
 
-    if (!this.connection) return;
+//     if (!this.connection) return;
 
-    await this.connection.invoke("StopSimulation");
+//     await this.connection.invoke("StopSimulation");
 
-  }
-}
+//   }
+// }
 
-export const stockSignalRService = new StockSignalRService();
+//export const stockSignalRService = new StockSignalRService();
