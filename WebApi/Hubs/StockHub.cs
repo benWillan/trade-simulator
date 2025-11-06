0b23037bb5a9c70adff070695ac2baa83cead755
Ben Willan <93150547+benWillan@users.noreply.github.com>
2025-11-06 17:01:20 +0000

    [chore] Rescaffolded with stock_fmp-data(um) table

--- WebApi/Hubs/StockHub.cs
+++ WebApi/Hubs/StockHub.cs
@@ -5,20 +5,20 @@ using WebApi.Services;
 using System.Threading;
 using Microsoft.EntityFrameworkCore;
 
-//using CoreLib.Context;
+using CoreLib.Context;
 using CoreLib.EFModels;
 
 namespace WebApi.Hubs;
 
 public class StockHub : Hub
 {
-    // private readonly MyDbContext _context;
-    //private static ConcurrentDictionary<string, CancellationTokenSource> _userStreams = new();
+     private readonly MyDbContext _context;
+    private static ConcurrentDictionary<string, CancellationTokenSource> _userStreams = new();
 
-    // public StockHub(MyDbContext dbContext)
-    // {
-    //     _context = dbContext;
-    // }
+     public StockHub(MyDbContext dbContext)
+     {
+         _context = dbContext;
+     }
     
     public async IAsyncEnumerable<List<string>> GetDataInChunks(
         int chunkSize,
@@ -41,52 +41,51 @@ public class StockHub : Hub
         }
     }
 
-    // public async IAsyncEnumerable<List<StockQuote>?> GetStockChunk(
-    //     string? ticker,
-    //     int chunkSize,
-    //     int delayMs,
-    //     DateTime? startDate,
-    //     [EnumeratorCancellation] CancellationToken cancellationToken)
-    // {
-    //     var stockData = await _context.Stocks
-    //         .AsNoTracking()
-    //         .Include(s => s.StockQuotes)
-    //         .Where(stock => stock.Ticker == ticker)
-    //         .Select(s => new Stock
-    //         {
-    //             StockQuotes = s.StockQuotes
-    //                 .Where(sq => !startDate.HasValue || sq.Date > startDate.Value.Date)
-    //                 .OrderBy(sq => sq.Date)
-    //                 .Select(sq => new StockQuote
-    //                 {
-    //                     StockSymbol = s.Ticker,
-    //                     Date = sq.Date,
-    //                     ClosePrice = sq.ClosePrice,
-    //                     OpenPrice = sq.OpenPrice,
-    //                     HighPrice = sq.HighPrice,
-    //                     LowPrice = sq.LowPrice,
-    //                     Volume = sq.Volume,
-    //                 })
-    //                 .ToList()
-    //         })
-    //         .ToListAsync();
-    //
-    //     for (var i = 0; i < stockData.FirstOrDefault()?.StockQuotes.Count; i += chunkSize)
-    //     {
-    //         if (cancellationToken.IsCancellationRequested)
-    //             yield break;
-    //         
-    //         if (delayMs > 0)
-    //             await Task.Delay(delayMs, cancellationToken);
-    //
-    //         //var chunk = stockData.FirstOrDefault()?.StockQuotes.ElementAt(i);
-    //         var chunk = stockData.FirstOrDefault()?.StockQuotes.Skip(i).Take(chunkSize).ToList();
-    //
-    //         if (chunk is null) yield break;
-    //         
-    //         yield return chunk;
-    //     }
-    // }
+    public async IAsyncEnumerable<List<StockQuote>?> GetStockChunk(
+        string? ticker,
+        int chunkSize,
+        int delayMs,
+        DateTime? startDate,
+        [EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        var stockData = await _context.Stocks
+            .AsNoTracking()
+            .Include(s => s.StockQuotes)
+            .Where(stock => stock.Ticker == ticker)
+            .Select(s => new Stock
+            {
+                StockQuotes = s.StockQuotes
+                    .Where(sq => !startDate.HasValue || sq.Date > startDate.Value.Date)
+                    .OrderBy(sq => sq.Date)
+                    .Select(sq => new StockQuote
+                    {
+                        StockSymbol = s.Ticker,
+                        Date = sq.Date,
+                        ClosePrice = sq.ClosePrice,
+                        OpenPrice = sq.OpenPrice,
+                        HighPrice = sq.HighPrice,
+                        LowPrice = sq.LowPrice,
+                        Volume = sq.Volume,
+                    })
+                    .ToList()
+            })
+            .ToListAsync();
     
+        for (var i = 0; i < stockData.FirstOrDefault()?.StockQuotes.Count; i += chunkSize)
+        {
+            if (cancellationToken.IsCancellationRequested)
+                yield break;
+            
+            if (delayMs > 0)
+                await Task.Delay(delayMs, cancellationToken);
+    
+            //var chunk = stockData.FirstOrDefault()?.StockQuotes.ElementAt(i);
+            var chunk = stockData.FirstOrDefault()?.StockQuotes.Skip(i).Take(chunkSize).ToList();
+    
+            if (chunk is null) yield break;
+            
+            yield return chunk;
+        }
+    }
     
 }
